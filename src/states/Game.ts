f2778cad9d9dc179a4329c2acc20a13dc8a51897
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-05 20:16:51 -0500

    Display inventory/message panel and living room

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,10 +1,28 @@
+interface IPlayerData {
+  room: string;
+}
+
+interface IRoomData {
+  background: string;
+}
+
 export class Game extends Phaser.State {
-  public preload() {
-    this.game.load.image('logo', 'images/phaser.png');
+  private playerData: IPlayerData;
+  private roomData: IRoomData;
+
+  public init(playerData: IPlayerData) {
+    this.playerData = playerData ? { ...playerData } : { room: 'livingroom' };
   }
 
   public create() {
-    const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    const panel = this.add.sprite(0, 270, 'panel');
+
+    this.loadRoom();
+  }
+
+  private loadRoom() {
+    this.roomData = JSON.parse(this.cache.getText(this.playerData.room));
+
+    const background = this.add.sprite(0, 0, this.roomData.background);
   }
 }

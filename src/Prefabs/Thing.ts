ca7b0902d4d2ca393c5f6f49e13bce5fd79f5836
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-06 18:27:49 -0500

    Display text property of item when it is clicked on

--- src/Prefabs/Thing.ts
+++ src/Prefabs/Thing.ts
@@ -1,16 +1,30 @@
+import { Game } from '../states/Game';
+
 export interface IThingData {
   asset: string;
+  text: string;
+  type: string;
   x: number;
   y: number;
 }
 
 export class Thing extends Phaser.Sprite {
-  private state: Phaser.State;
+  public data: IThingData;
+  private state: Game;
 
-  constructor(state: Phaser.State, data: IThingData) {
+  constructor(state: Game, data: IThingData) {
     super(state.game, data.x, data.y, data.asset);
 
+    this.data = data;
     this.state = state;
     this.anchor.setTo(0.5);
+
+    this.inputEnabled = true;
+    this.input.pixelPerfectClick = true;
+    this.events.onInputDown.add(this.touch, this);
+  }
+
+  private touch() {
+    this.state.panelLabel.text = this.data.text;
   }
 }
